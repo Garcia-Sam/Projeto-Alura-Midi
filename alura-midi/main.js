74a911350708497fe7689878eaf37abc905f30b5
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-10-05 14:56:33 -0300

    Update main.js

--- alura-midi/main.js
+++ alura-midi/main.js
@@ -10,9 +10,12 @@ for (let contador = 0; contador < listasDeTeclas.length; contador++){
     const tecla = listasDeTeclas[contador]
     const instrumento = tecla.classList[1]
     const idAudio = `#som_${instrumento}` //template string
-    //console.log(idAudio)
 
     tecla.onclick = function (){
         tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa')
     }
 }
